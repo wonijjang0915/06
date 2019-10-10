27b8731603498a0a485884fafc8596d42c06015e
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-10-10 16:01:35 +0900

    03

--- main.c
+++ main.c
@@ -23,5 +23,18 @@ int get_max(int a, int b)
 
 int main ()
 {
-return 0;
+	int x, y;
+	int output;
+	
+	x=2;
+	y=5;
+	
+	output= sumTwo(x,y);
+	printf("sumTwo : %i\n", output);
+	printf("square : %i\n", square(x));
+	printf("get_max : %i\n", get_max(x,y));
+	
+	
+	
+    return 0;
 }
