6be51428fa80fa6ea0f0b85b8f96854532dc9a78
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-10-15 16:13:43 +0900

    07

--- main.c
+++ main.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int factorial(int n)
+{
+	int result=1;
+	int i;
+	for(i=1;i<=n;i++)
+	{ 
+	   result=result*i;
+	   }  
+	return result;
+}
+
+int calcCombination(int n, int r)
+{
+	int high, low;
+	high = factorial(n);
+	low = factorial(n-r)*factorial(r);
+	
+	return high/low;
+}
 
 int main(void)
 {
-	int i;
-	int max=45;
+	int n, r;
+	int result;
 	
-	srand((unsigned)time(NULL));
-	for(i=0; i<6; i++)
-	   printf("%d", 1+rand()%max);
-	   
-	return 0;
+    printf("input n and r");
+    scanf("%d %d", &n, &r);
+    
+	result = calcCombination(n, r);
+	printf("combination result is %d\n", result);
 	
+    return 0;	
 }
